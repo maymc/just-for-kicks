003bff573ab96f56c2b86292061b15502dec465d
May Chen <maychen@Mays-MacBook-Pro.local>
2018-06-21 19:38:51 -1000

    Completed ex1

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 // 1. Add an event to the 'See Details' content that will create an alert box that says 'Not Available in Hawaii.' after clicking on See Details.
+var detailsElem = document.getElementById("details");
+detailsElem.addEventListener('click', alertBox);
 
+function alertBox(){
+    alert("Not Available in Hawaii");
+}
 
 //2. Add an event to the div element with the id of 'name1' that will show/hide the description ('descrip1') after hovering over Air Jordan II.
 
